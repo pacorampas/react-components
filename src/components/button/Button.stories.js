3f0269591310b75bf4b77d436ccb07fede2981a4
Paco Rampas <pacorampas@gmail.com>
2021-08-14 10:44:53 +0200

    chore: add first story

---
+++ src/components/button/Button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button, ButtonProps } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+};
+
+export const PrimaryButton = () => <Button label="Hello world" primary />;
