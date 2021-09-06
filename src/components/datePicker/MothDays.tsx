302be1a06261dedbf7575f76068160645216997d
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2021-09-06 21:40:02 +0200

    feat: add linter and prettier

--- src/components/datePicker/MothDays.tsx
+++ src/components/datePicker/MothDays.tsx
@@ -1,40 +1,40 @@
-import React from "react";
-import styled from "styled-components";
-import { DateTime } from "luxon";
+import React from 'react'
+import styled from 'styled-components'
+import { DateTime } from 'luxon'
 
 interface DayProps {
-  selected?: boolean;
-  outOfMont?: boolean;
-  tabIndex: string | number;
+  selected?: boolean
+  outOfMont?: boolean
+  tabIndex: string | number
 }
 
 export type DayArguments = {
-  day: number;
-  ts: number;
-  date: Date;
-  prevMonth: boolean;
-  nextMonth: boolean;
-};
+  day: number
+  ts: number
+  date: Date
+  prevMonth: boolean
+  nextMonth: boolean
+}
 
 export interface MonthDaysProps {
-  selected?: Date;
-  month?: Date;
-  fillEmptyDays?: boolean;
-  onChange?: (arg: DayArguments) => void;
+  selected?: Date
+  month?: Date
+  fillEmptyDays?: boolean
+  onChange?: (arg: DayArguments) => void
 }
 
 const Wrapper = styled.div`
   width: 100%;
   box-sizing: border-box;
   border: solid 1px;
-`;
+`
 
 const Row = styled.div`
   display: flex;
-`;
+`
 
 const Day = styled.button.attrs(({ tabIndex = -1 }: DayProps) => {
-  return { tabIndex };
+  return { tabIndex }
 })`
   display: flex;
   justify-content: center;
@@ -46,11 +46,11 @@ const Day = styled.button.attrs(({ tabIndex = -1 }: DayProps) => {
   height: 30px;
   box-sizing: border-box;
   border-radius: 6px;
-  opacity: ${({ outOfMont }: DayProps) => (outOfMont ? "0.5" : "1")};
-  background: ${({ selected }: DayProps) => (selected ? "#eee" : "none")};
-`;
+  opacity: ${({ outOfMont }: DayProps) => (outOfMont ? '0.5' : '1')};
+  background: ${({ selected }: DayProps) => (selected ? '#eee' : 'none')};
+`
 
-const defaultDate = new Date();
+const defaultDate = new Date()
 
 export const MonthDays = ({
   selected = defaultDate,
@@ -58,115 +58,112 @@ export const MonthDays = ({
   fillEmptyDays = false,
   onChange = undefined,
 }: MonthDaysProps) => {
-  const luxonDate = DateTime.fromJSDate(month || selected).startOf("day");
-  const luxonSelectedDate = DateTime.fromJSDate(selected).startOf("day");
-  const firstDayOfMonth = luxonDate.set({ day: 1 });
-  const lastDayOfMonth = luxonDate.set({ day: firstDayOfMonth.daysInMonth });
+  const luxonDate = DateTime.fromJSDate(month || selected).startOf('day')
+  const luxonSelectedDate = DateTime.fromJSDate(selected).startOf('day')
+  const firstDayOfMonth = luxonDate.set({ day: 1 })
+  const lastDayOfMonth = luxonDate.set({ day: firstDayOfMonth.daysInMonth })
 
   const renderDays = () => {
-    const fillMonthBeforeWith = firstDayOfMonth.weekday;
-    const totalDaysInMonth = luxonDate.daysInMonth;
-    const fillMonthAfterWith = 7 - lastDayOfMonth.weekday;
+    const fillMonthBeforeWith = firstDayOfMonth.weekday
+    const totalDaysInMonth = luxonDate.daysInMonth
+    const fillMonthAfterWith = 7 - lastDayOfMonth.weekday
 
-    const daysArray = [];
-    let i = 1;
+    const daysArray = []
+    let i = 1
     while (i < fillMonthBeforeWith) {
-      const diff = fillMonthBeforeWith - i;
-      const date = firstDayOfMonth.minus({ days: diff });
+      const diff = fillMonthBeforeWith - i
+      const date = firstDayOfMonth.minus({ days: diff })
       daysArray.push({
-        ts: date.toFormat("x"),
+        ts: date.toFormat('x'),
         day: date.day,
         date: date.toJSDate(),
         prevMonth: true,
         nextMonth: false,
-      });
-      i += 1;
+      })
+      i += 1
     }
 
-    i = 0;
+    i = 0
     while (i < totalDaysInMonth) {
-      const date = firstDayOfMonth.plus({ days: i });
+      const date = firstDayOfMonth.plus({ days: i })
       daysArray.push({
-        ts: date.toFormat("x"),
+        ts: date.toFormat('x'),
         day: date.day,
         date: date.toJSDate(),
         prevMonth: false,
         nextMonth: false,
-      });
-      i += 1;
+      })
+      i += 1
     }
 
-    i = 1;
+    i = 1
     while (i <= fillMonthAfterWith) {
-      const date = lastDayOfMonth.plus({ days: i });
+      const date = lastDayOfMonth.plus({ days: i })
       daysArray.push({
-        ts: date.toFormat("x"),
+        ts: date.toFormat('x'),
         day: date.day,
         date: date.toJSDate(),
         prevMonth: false,
         nextMonth: true,
-      });
-      i += 1;
+      })
+      i += 1
     }
 
-    return daysArray;
-  };
+    return daysArray
+  }
 
   const daysInRows = ({ daysToRender }: { daysToRender: any[] }) => {
-    const rows: { [key: number]: any[] } = {};
-    let rowIndex: number = -1;
+    const rows: { [key: number]: any[] } = {}
+    let rowIndex: number = -1
     daysToRender.forEach((day, i) => {
       if (i % 7 === 0) {
-        rowIndex += 1;
+        rowIndex += 1
       }
 
       if (!rows[rowIndex]) {
-        rows[rowIndex] = [];
+        rows[rowIndex] = []
       }
 
-      rows[rowIndex].push(day);
-    });
+      rows[rowIndex].push(day)
+    })
 
-    return rows;
-  };
+    return rows
+  }
 
-  const daysToRender = renderDays();
-  const rows = daysInRows({ daysToRender });
+  const daysToRender = renderDays()
+  const rows = daysInRows({ daysToRender })
 
   const handleDayClick = function (this: DayArguments) {
-    onChange?.(this);
-  };
+    onChange?.(this)
+  }
 
   return (
     <Wrapper>
       {Object.keys(rows).map((key: any) => {
-        const row = rows[key];
+        const row = rows[key]
         return (
           <Row key={key}>
             {row.map((day: DayArguments) => {
-              const isOutDay = day.prevMonth || day.nextMonth;
+              const isOutDay = day.prevMonth || day.nextMonth
 
               if (!fillEmptyDays && isOutDay) {
-                return <Day key={day.ts} disabled />;
+                return <Day key={day.ts} disabled />
               }
 
               return (
                 <Day
                   key={day.ts}
-                  selected={
-                    luxonSelectedDate.toMillis() ===
-                    DateTime.fromJSDate(day.date).startOf("day").toMillis()
-                  }
+                  selected={luxonSelectedDate.toMillis() === DateTime.fromJSDate(day.date).startOf('day').toMillis()}
                   outOfMont={day.prevMonth || day.nextMonth}
                   onClick={handleDayClick.bind(day)}
                 >
                   {day.day}
                 </Day>
-              );
+              )
             })}
           </Row>
-        );
+        )
       })}
     </Wrapper>
-  );
-};
+  )
+}
