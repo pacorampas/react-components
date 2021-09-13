36ceae352da0618188e97f41ed20a2db34afccdd
Manuel Casas Barrado <manuel.casasbarrado@gmail.com>
2021-09-13 19:00:31 +0200

    feat: add button tests

--- src/components/button/Button.test.tsx
+++ src/components/button/Button.test.tsx
@@ -1,14 +1,51 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-
+import { render, fireEvent } from '@testing-library/react'
+import 'jest-styled-components'
 import { Button } from './Button'
+import { VARIANT } from '../../theme'
 
-describe('<Button />', () => {
-  test('should print label text', async () => {
-    const { getByRole } = render(<Button>Call to action</Button>)
+const renderButton = (props = {}) => {
+  const { getByRole, ...rest } = render(<Button {...props}>Call to action</Button>)
+  return { button: getByRole('button'), getByRole, ...rest }
+}
 
-    const button = getByRole('button')
+test('renders correctly', () => {
+  const { button } = renderButton()
+  expect(button).toBeDefined()
+  expect(button).toBeInTheDocument()
+  expect(button).toHaveTextContent('Call to action')
+})
 
-    expect(button).toContainHTML('Call to action')
+describe('Button', () => {
+  describe('with modifiers', () => {
+    test('should render a bordered button', async () => {
+      const { button } = renderButton({ bordered: true })
+
+      expect(button).toMatchSnapshot()
+    })
+
+    test('should render a solid button', async () => {
+      const { button } = renderButton({ bordered: false })
+
+      expect(button).toMatchSnapshot()
+    })
+
+    Object.values(VARIANT).forEach((variant) => {
+      test(`should render using ${variant}`, async () => {
+        const { button } = renderButton({ variant })
+
+        expect(button).toMatchSnapshot()
+      })
+    })
+  })
+
+  describe('with interaction', () => {
+    test('trigger onClick prop when user clicks on the button', () => {
+      const onDummyClick = jest.fn()
+      const { button } = renderButton({ onClick: onDummyClick })
+
+      fireEvent.click(button)
+      expect(onDummyClick).toHaveBeenCalledTimes(1)
+    })
   })
 })
