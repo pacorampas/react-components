caa946f132359e1f0f25295c3f7e001d9fb0a25b
Paco Rampas <pacorampas@gmail.com>
2021-09-13 18:42:46 +0200

    feat: add theme provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,19 +1,25 @@
-import { withTests } from "@storybook/addon-jest";
+import { withTests } from '@storybook/addon-jest'
+import { ThemeProvider } from 'styled-components'
+import { withThemesProvider } from 'storybook-addon-styled-component-theme'
+import { addDecorator } from '@storybook/react'
+import { themes } from '../src/theme'
 
-import results from "../.jest-test-results.json";
+import results from '../.jest-test-results.json'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-};
+}
+
+addDecorator(withThemesProvider(themes), ThemeProvider)
 
 export const decorators = [
   withTests({
     results,
   }),
-];
+]
